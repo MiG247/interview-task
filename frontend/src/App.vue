2468f305d0f84bed2d8b1b5247da7a9fa6a759b8
MiG247 <michaelk247@yahoo.de>
2021-01-03 10:15:23 +0100

    added client

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="container">
+    <h2 style="text-align:center; color:grey; font-weight:inherit;">TO-DO List</h2>
+    <task-list/>
+  </div>
+</template>
+
+<script>
+
+import TaskList from './components/TaskList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TaskList
+  }
+}
+</script>
+
+<style>
+.container{
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+
+</style>
